d3bcb1ee4ddea14d5c2cc6ac5e3466571b6bd2d3
tentationem <spinel1@mail.ru>
2025-04-14 15:01:52 +0700

    Additional includes(sfml); testing sfml events and text-fonts

--- src/headers/sfml-headers.h
+++ src/headers/sfml-headers.h
@@ -1,6 +1,13 @@
 #pragma once
+
+//Defines
+#define WINDOW_TITLE "Platformer"
+
+
 //C libraries
 #include <iostream>
 
 //SFML
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,49 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({800u, 800u}), "Window", sf::Style::Titlebar | sf::Style::Close);
+    //Loading font
+    sf::Font font;
+    if(font.openFromFile("fonts/Roboto_Condensed-Black.ttf"))
+        std::cout << "Font opened" << std::endl;
+    else
+        std::cout << "Font error" << std::endl;
+
+    //Loading text
+    sf::Text txt(font, "uwu zombe (o'w'o)");
+    txt.setPosition(sf::Vector2f(320.0f,350.0f));
+    txt.setFillColor(sf::Color::White);
+    
+
+
+    //Window preferences
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 800u}), WINDOW_TITLE, sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(144);
 
+
+    //Main loop
     while (window.isOpen())
     {
+
         while (const std::optional event = window.pollEvent())
         {
+            //Close window on "Close" button
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
+
+            if(event->is<sf::Event::MouseWheelScrolled>())
+            {
+                std::cout << "MouseWheelScrolled" << std::endl;
+            }
+            
         }
 
-        window.clear();
+        window.clear(sf::Color(10u,20u,30u,100u));
+        window.draw(txt);
         window.display();
     }
+
+
+
 }
