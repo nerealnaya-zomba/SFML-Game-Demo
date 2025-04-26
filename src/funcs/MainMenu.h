aabda77a107fb2ff2c554ed1f397cd97487054ff
tentationem <spinel1@mail.ru>
2025-04-27 02:45:06 +0700

    added triggers on main menu's buttons, animation for exit button

---
+++ src/funcs/MainMenu.h
@@ -0,0 +1,124 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <ctime>
+#include <iostream>
+
+void moveRectToMouse(sf::RectangleShape& rect,sf::RenderWindow& window)
+{
+    rect.setPosition({sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y});
+}
+
+void rainbowWindowClear(sf::RenderWindow& window, sf::Color& backgroundColor)
+{
+    srand(time(0));
+
+    static bool rGoUp = true;
+    static bool rGoDown = false;
+    if(rGoUp)
+    {
+        backgroundColor.r+=rand()%2+1;
+
+        if(backgroundColor.r>249)
+        {
+            rGoUp = false;
+            rGoDown = true;
+        }
+    }
+    else if(rGoDown)
+    {
+        backgroundColor.r-=rand()%2+1;
+
+        if(backgroundColor.r<6)
+        {
+            rGoUp = true;
+            rGoDown = false;
+        }
+    }
+    /////////////////////////////////////////////////
+    static bool gGoUp = true;
+    static bool gGoDown = false;
+    if(gGoUp)
+    {
+        backgroundColor.g+=rand()%2+1;
+
+        if(backgroundColor.g>249)
+        {
+            gGoUp = false;
+            gGoDown = true;
+        }
+    }
+    else if(gGoDown)
+    {
+        backgroundColor.g-=rand()%2+1;
+
+        if(backgroundColor.g<6)
+        {
+            gGoUp = true;
+            gGoDown = false;
+        }
+    }
+
+    static bool bGoUp = true;
+    static bool bGoDown = false;
+    if(bGoUp)
+    {
+        backgroundColor.b+=rand()%2+1;
+
+        if(backgroundColor.b>249)
+        {
+            bGoUp = false;
+            bGoDown = true;
+        }
+    }
+    else if(bGoDown)
+    {
+        backgroundColor.b-=rand()%2+1;
+
+        if(backgroundColor.b<6)
+        {
+            bGoUp = true;
+            bGoDown = false;
+        }
+    }
+    //std::cout << (int)backgroundColor.r << " " << (int)backgroundColor.g << " " << (int)backgroundColor.b << " " << std::endl;
+    window.clear(backgroundColor);
+}
+
+//Size up rectangle when mouseRect based on mousePos intersects rect
+void sizeUpRectangleOnHover(sf::RectangleShape& rect, sf::RectangleShape& mouseRect, sf::RenderWindow& window, float sizingUpSpeed=0.05f, float sizingDownSpeed=0.05f)
+{
+    bool isSizeUp = false;
+    bool isSizeDown = false;
+    mouseRect.setPosition({sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y});
+    //std::cout << "mouseRect: " << mouseRect.getGlobalBounds().position.x << "" << mouseRect.getGlobalBounds().position.y << "     "<< "rect: " << rect.getGlobalBounds().position.x << "" << rect.getGlobalBounds().position.y << std::endl;
+
+    if(mouseRect.getGlobalBounds().findIntersection(rect.getGlobalBounds()))
+    {
+        isSizeUp = true;
+        
+    }
+    else
+    {
+        isSizeUp = false;
+        isSizeDown = true;
+    }
+
+    if(isSizeUp && (rect.getScale().x <=1.3f && rect.getScale().y <= 1.3f))
+    {
+        rect.setScale({rect.getScale().x+sizingUpSpeed, rect.getScale().y+sizingUpSpeed});
+    }
+    else
+    {
+        isSizeUp = false;
+    }
+
+    if(isSizeDown && (rect.getScale().x >=1.05f && rect.getScale().y >= 1.05f))
+    {
+        rect.setScale({rect.getScale().x-sizingDownSpeed, rect.getScale().y-sizingDownSpeed});
+    }
+    else
+    {
+        isSizeDown = false;
+    }
+    
+}
---
+++ src/sfml-headers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//Local includes
+#include <MainMenu.h>
+
+//Defines
+#define WINDOW_TITLE "Platformer"
+#define WINDOW_HEIGHT 800u
+#define WINDOW_WIDTH 800u
+#define WINDOW_FPS 144u
+
+//C libraries
+#include <iostream>
+
+//SFML
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
