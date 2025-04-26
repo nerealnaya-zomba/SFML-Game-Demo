eb80814561ada39ed5313f12f88f3d28e8061377
tentationem <spinel1@mail.ru>
2025-04-26 19:48:22 +0700

    added main menu, main menu background, main menu buttons, and some funcs(for scaling up buttons on mouse hover and func for rainbow main menu background)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@ int main()
     //Window preferences
     auto window = sf::RenderWindow(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), WINDOW_TITLE, sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(144);
+
+    //Bools
+    bool isMainMenuCalled = true;
+
+    //Mouse
+    sf::RectangleShape mouseRect({1.f,1.f});
+
+    //Background
+    sf::Color backGroundColor({0,0,0});
     
     //Loading font
     sf::Font font;
@@ -13,22 +22,42 @@ int main()
     else
         std::cout << "Font error" << std::endl;
 
-    //Loading text
-    sf::Text txt(font, "uwu zombe (o'w'o)");
-    txt.setPosition(sf::Vector2f(320.0f,350.0f));
-    txt.setFillColor(sf::Color::White);
+    //Main menu
+    sf::RectangleShape playButton(sf::Vector2f{200.f,75.f});
+    playButton.setFillColor(sf::Color::Black);
+    playButton.setOrigin({playButton.getSize().x/2,playButton.getSize().y/2});
+    playButton.setPosition({WINDOW_WIDTH/2,WINDOW_HEIGHT-325+50});
+    sf::Text playButtonText(font,"Play");
+    playButtonText.setFillColor(sf::Color::White);
+    playButtonText.setOrigin({playButtonText.getGlobalBounds().size.x/2,playButtonText.getGlobalBounds().size.y/2});
+    playButtonText.setPosition({playButton.getPosition().x, playButton.getPosition().y-2});
+    sf::RectangleShape exitButton(sf::Vector2f{200.f,75.f});
+    exitButton.setFillColor(sf::Color::Black);
+    exitButton.setOrigin({exitButton.getSize().x/2,exitButton.getSize().y/2});
+    exitButton.setPosition({WINDOW_WIDTH/2,WINDOW_HEIGHT-200+50});
+    sf::Text exitButtonText(font,"Exit");
+    exitButtonText.setFillColor(sf::Color::White);
+    exitButtonText.setOrigin({exitButtonText.getGlobalBounds().size.x/2,exitButtonText.getGlobalBounds().size.y/2});
+    exitButtonText.setPosition({exitButton.getPosition().x, exitButton.getPosition().y-6});
+
     
-    
-    
-    
+
 
 
     //Main loop
     while (window.isOpen())
     {
+        
+
 
         while (const std::optional event = window.pollEvent())
         {
+            if(isMainMenuCalled)
+            {
+                
+            }
+
+
             //Close window on "Close" button
             if (event->is<sf::Event::Closed>())
             {
@@ -41,11 +70,32 @@ int main()
             }
             
         }
+        
+        //Main menu
+        if(isMainMenuCalled)
+        {
+
+
+
+
+
+            sizeUpRectangleOnHover(playButton,mouseRect,window, 0.03f , 0.02f);
+            sizeUpRectangleOnHover(exitButton,mouseRect,window, 0.03f , 0.02f);
+
+            rainbowWindowClear(window, backGroundColor);
+
+            window.draw(playButton);
+            window.draw(exitButton);
+            window.draw(playButtonText);
+            window.draw(exitButtonText);
+            window.display();
+            continue;
+        }
 
         try
         {
             window.clear(sf::Color(10u,20u,30u,100u));
-            window.draw(txt);
+            
             window.display();
         }
         catch(const std::exception& e)
