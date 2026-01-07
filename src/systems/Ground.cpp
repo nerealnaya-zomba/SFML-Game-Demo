76c82473d1aabc1c866f7db0c0fdfc1b371b0e71
nerealnaya-zomba <spinel1@mail.ru>
2026-01-07 09:50:59 +0700

    static casting levelSIze

--- src/systems/Ground.cpp
+++ src/systems/Ground.cpp
@@ -19,7 +19,7 @@ Ground::Ground(GameData& gameTextures, GameLevel& level, std::string groundFileN
     ground1Rect_m = new sf::RectangleShape();
 
     ground1Rect_m->setSize({
-        level.getLevelSize().x,
+        static_cast<float>(level.getLevelSize().x),
         static_cast<float>(ground1Sprite_m->getTexture().getSize().y
     )});
 
