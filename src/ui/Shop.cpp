530334fc6fdd25ea35ffdcbf2236110cd6191ee8
nerealnaya-zomba <spinel1@mail.ru>
2026-01-10 14:09:55 +0700

    Shop inheritance Interactive object, and some implementions

--- src/ui/Shop.cpp
+++ src/ui/Shop.cpp
@@ -1,1 +1,41 @@
 #include<Shop.h>
+
+void Shop::update()
+{
+    if(!isOpened) return;
+}
+
+void Shop::handleEvent(const sf::Event &event)
+{
+    if(!isOpened) return;
+}
+
+void Shop::draw(sf::RenderWindow& window)
+{
+    if(!isOpened) return;
+
+    window.draw(*sprite);
+
+    for (auto &&item : items)
+    {
+        // TODO
+        // item.draw(window)
+    }
+    
+}
+
+Shop::Shop(GameData& d, Player& p, sf::Vector2f pos)
+    : InteractiveObject(pos), isOpened(false), data(&d), player(&p)
+{
+
+}
+
+void Shop::open()
+{
+    isOpened = true;
+}
+
+void Shop::close()
+{
+    isOpened = false;
+}
--- src/ui/Shop.h
+++ src/ui/Shop.h
@@ -1,18 +1,29 @@
 #pragma once
 #include<SFML/Graphics.hpp>
 #include<Item.h>
+#include<Player.h>
+#include<GameData.h>
 #include<vector>
+#include<Interactive.h>
 
-class Shop
+class Shop : InteractiveObject
 {
 private:
+    Player* player;
+    GameData* data;
+
     std::vector<std::shared_ptr<Item>> items;
 
-    
+    bool isOpened;
     
 public:
-
-    void update();
-    void draw();
+    Shop(GameData& d, Player& p, sf::Vector2f pos);
+    ~Shop() = default;
+    void open();
+    void close();
     
+
+    void draw(sf::RenderWindow& window)         override;
+    void update()                               override;
+    void handleEvent(const sf::Event& event)    override;
 }; 
