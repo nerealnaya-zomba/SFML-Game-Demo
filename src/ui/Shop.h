6609e01c2ac1a3a631396e98bdd5c421105cad3f
nerealnaya-zomba <spinel1@mail.ru>
2026-01-10 12:56:07 +0700

    some implementions

--- src/ui/Shop.h
+++ src/ui/Shop.h
@@ -1,10 +1,18 @@
 #pragma once
 #include<SFML/Graphics.hpp>
+#include<Item.h>
+#include<vector>
 
 class Shop
 {
 private:
+    std::vector<std::shared_ptr<Item>> items;
 
+    
+    
 public:
 
-};
+    void update();
+    void draw();
+    
+}; 
