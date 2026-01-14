8c238c87bbdc1edf4c63668cc0d5e6ff97ddcb16
nerealnaya-zomba <spinel1@mail.ru>
2026-01-14 22:47:17 +0700

    ItemWidget works fine

--- src/ui/Shop.cpp
+++ src/ui/Shop.cpp
@@ -3,13 +3,21 @@
 void Shop::update()
 {
     if(!isOpened) return;
+
     alignItemsOnGrid();
     onItemSelected();
+
+    widget.update();
 }
 
 void Shop::handleEvent(const sf::Event &event)
 {
-    if(!isOpened) return;
+    if(widget.getIsOpened())
+    {
+        widget.handleEvent(event);
+        return;
+    }
+
 
     if(const auto* keyPressed = event.getIf<sf::Event::KeyPressed>())
     {
@@ -21,7 +29,13 @@ void Shop::handleEvent(const sf::Event &event)
         {
             moveSelectionLeft();
         }
+        else if(keyPressed->scancode == SHOP_KEY_TO_OPEN_ITEM_WIDGET)
+        {
+            openSelectedItemWidget();
+        }
     }
+
+
 }
 
 bool Shop::getIsOpened()
@@ -38,10 +52,10 @@ void Shop::draw(sf::RenderWindow& window)
 
     for (auto &&item : items)
     {
-        item.second->draw(window);
-        window.draw(*(item.first));
+        item->draw(window);
     }
     
+    widget.draw(window);
 }
 
 void Shop::initializeItems()
@@ -137,7 +151,7 @@ void Shop::alignItemsOnGrid()
 
         sf::Vector2i convertedNextPos = static_cast<sf::Vector2i>(nextPos);
 
-        item.second->setPosition(convertedNextPos);
+        item->setPosition(convertedNextPos);
 
         iterationCount++;
 
@@ -151,15 +165,15 @@ void Shop::alignItemsOnGrid()
 
 void Shop::onItemSelected()
 {
-    itemsIt->second->setScale({
-        itemsIt->second->getBaseScale().x*BASE_SHOP_ITEM_SCALEUP_MULTIPLY_ON_HOVER.x,
-        itemsIt->second->getBaseScale().y*BASE_SHOP_ITEM_SCALEUP_MULTIPLY_ON_HOVER.y
+    itemsIt->get()->setScale({
+        itemsIt->get()->getBaseScale().x*BASE_SHOP_ITEM_SCALEUP_MULTIPLY_ON_HOVER.x,
+        itemsIt->get()->getBaseScale().y*BASE_SHOP_ITEM_SCALEUP_MULTIPLY_ON_HOVER.y
     });
 }
 
 void Shop::onSelectedChanged()
 {
-    itemsIt->second->setScale(itemsIt->second->getBaseScale());
+    itemsIt->get()->setScale(itemsIt->get()->getBaseScale());
 }
 
 void Shop::moveSelectionRight()
@@ -194,9 +208,25 @@ void Shop::unblockPlayerControl()
     this->player->unblockControls();
 }
 
+void Shop::openSelectedItemWidget()
+{
+    isItemWidgetOpened = true;
+    widget.attachItemStats(*itemsIt->get());
+    widget.open();
+}
+
+void Shop::closeSelectedItemWidget()
+{
+    isItemWidgetOpened = false;
+    widget.close();
+}
+
 Shop::Shop(GameData &d, Player &p, sf::Vector2f pos)
     : InteractiveObject(pos, d.bulletTextures[0]), isOpened(false), data(&d), player(&p), columns(BASE_SHOP_COLUMNS),
-      rows(BASE_SHOP_ROWS), cellSize(BASE_SHOP_CELL_SIZE)
+      rows(BASE_SHOP_ROWS), 
+      cellSize(BASE_SHOP_CELL_SIZE), 
+      isItemWidgetOpened(false), 
+      widget({BASE_SHOP_BACKGROUND_SIZE.x, BASE_SHOP_BACKGROUND_SIZE.y},pos,{0,0},{0.f,BASE_SHOP_BACKGROUND_SIZE.y}, *d.gameFont, (BASE_SHOP_BACKGROUND_SIZE.x+BASE_SHOP_BACKGROUND_SIZE.y)/20, (BASE_SHOP_BACKGROUND_SIZE.x+BASE_SHOP_BACKGROUND_SIZE.y)/16)
 {
     shopBackground.setSize(BASE_SHOP_BACKGROUND_SIZE);
     setRectangleOriginToMiddle(shopBackground);
@@ -224,14 +254,164 @@ void Shop::close()
 
 void Shop::addItem(std::unique_ptr<Item> item)
 {
-    std::unique_ptr<sf::Text> text = std::make_unique<sf::Text>
-    (
-        *data->gameFont,
-        item->displayName
-    );
+    items.push_back(std::move(item));
+}
 
-    items.emplace(
-        std::move(text),
-        std::move(item)
+Shop::ItemWidget::ItemWidget(sf::Vector2f widgetSize, sf::Vector2f widgetPos,sf::Vector2f displayNamePosLocal, sf::Vector2f statsTextPosLocal, sf::Font& font, uint8_t displayNameSize, uint8_t statsTextSize)
+    : displayNameText(font,"",displayNameSize), statsText(font,"",statsTextSize), isOpened(false)
+{
+    // Background init
+    rect.setFillColor(sf::Color::Blue);
+    rect.setSize(widgetSize);
+    setRectangleOriginToMiddle(rect);
+    
+
+    this->displayNameText.setCharacterSize(displayNameSize);
+    this->statsText.setCharacterSize(statsTextSize);
+    setDisplayNamePosition(displayNamePosLocal);
+    setStatsTextPosition(statsTextPosLocal);
+
+    setWidgetCenterPosition(widgetPos);
+}
+
+void Shop::ItemWidget::draw(sf::RenderWindow &window)
+{
+    if(!isOpened) return;
+    
+    window.draw(this->rect);
+    window.draw(this->displayNameText);
+    window.draw(this->statsText);
+}
+
+void Shop::ItemWidget::update()
+{
+    if(!isOpened) return;
+}
+
+void Shop::ItemWidget::handleEvent(const sf::Event &event)
+{
+    if(!isOpened) return;
+
+    //При нажатии на кнопку
+    if(const auto* keyPressed = event.getIf<sf::Event::KeyPressed>())
+    {
+        // Активировать выделенную кнопку
+        if(keyPressed->scancode == sf::Keyboard::Scancode::Z)
+        {
+            buy(); 
+        }
+        else if(keyPressed->scancode == sf::Keyboard::Scancode::X)
+        {
+            close();
+        }
+    }
+}
+
+void Shop::ItemWidget::open()
+{
+    isOpened = true;
+}
+
+void Shop::ItemWidget::close()
+{
+    isOpened = false;
+}
+
+void Shop::ItemWidget::buy()
+{
+
+    close();
+}
+
+void Shop::ItemWidget::cancel()
+{
+
+    close();
+}
+
+bool Shop::ItemWidget::getIsOpened()
+{
+    return this->isOpened;
+}
+
+void Shop::ItemWidget::attachItemStats(Item& item)
+{
+    displayNameText.setString(item.displayName);
+    statsText.setString(
+        "BulletDistance: +"     + std::to_string(item.stats.bulletDistance)                 + "\n"+
+        "BulletSpeed: +"        + std::to_string(item.stats.bulletSpeed)                    + "\n"+
+        "BulletDamage: +"       + std::to_string(item.stats.damage)                         + "\n"+
+        "Health: +"             + std::to_string(item.stats.health)                         + "\n"+
+        "InitialSpeed: +"       + std::to_string(item.stats.initialSpeed)                   + "\n"+
+        "MaxSpeed: +"           + std::to_string(item.stats.maxSpeed)                       + "\n"+
+        "Shoot cooldown: -"     + std::to_string(item.stats.shootSpeedCooldownReduction)    + "\n"
     );
+}
+
+void Shop::ItemWidget::setWidgetSize(sf::Vector2f size)
+{
+    rect.setSize(size);
+    setRectangleOriginToMiddle(rect);
+}
+
+void Shop::ItemWidget::setDisplayNameText(std::string str)
+{
+    this->displayNameText.setString(str);
+}
+
+void Shop::ItemWidget::setStatsText(std::string str)
+{
+    this->statsText.setString(str);
+}
+
+void Shop::ItemWidget::setDisplayNamePosition(sf::Vector2f position)
+{
+    sf::Vector2f rectLeftTopPos = {
+        rect.getPosition().x-(rect.getSize().x/2),
+        rect.getPosition().y-(rect.getSize().y/2)
+    };
+    
+    this->displayNameText.setPosition({
+        rectLeftTopPos.x+position.x,
+        rectLeftTopPos.y+position.y
+    });
+}
+
+void Shop::ItemWidget::setStatsTextPosition(sf::Vector2f position)
+{
+    sf::Vector2f rectLeftTopPos = {
+        rect.getPosition().x-(rect.getSize().x/2),
+        rect.getPosition().y-(rect.getSize().y/2)
+    };
+    
+    this->statsText.setPosition({
+        rectLeftTopPos.x+position.x,
+        rectLeftTopPos.y+position.y
+    });
+}
+
+void Shop::ItemWidget::setWidgetCenterPosition(sf::Vector2f position)
+{
+    // Getting difference
+    sf::Vector2f positionDiff = {
+        -(rect.getPosition().x-position.x),
+        -(rect.getPosition().y-position.y)
+    };
+
+    // Rect positioning
+    rect.setPosition(position);
+
+    // DisplayName positioning
+    sf::Vector2f displayNamePos = displayNameText.getPosition();
+    displayNameText.setPosition({
+        displayNamePos.x+positionDiff.x,
+        displayNamePos.y+positionDiff.y
+    });
+
+    // StatsText positioning
+    sf::Vector2f statsTextPos = statsText.getPosition();
+    statsText.setPosition({
+        statsTextPos.x+positionDiff.x,
+        statsTextPos.y+positionDiff.y
+    });
 }
--- src/ui/Shop.h
+++ src/ui/Shop.h
@@ -6,23 +6,65 @@
 #include<vector>
 #include<Interactive.h>
 
-const unsigned int BASE_SHOP_COLUMNS          = 5;
-const unsigned int BASE_SHOP_ROWS             = 4;
-const sf::Vector2f BASE_SHOP_CELL_SIZE        = {40,40};
-const sf::Vector2f BASE_SHOP_BACKGROUND_SIZE  = {200,200};
-const sf::Vector2f BASE_SHOP_PADDING          = {20,20};
+const unsigned int BASE_SHOP_COLUMNS                        = 5;
+const unsigned int BASE_SHOP_ROWS                           = 4;
+const sf::Vector2f BASE_SHOP_CELL_SIZE                      = {40,40};
+const sf::Vector2f BASE_SHOP_BACKGROUND_SIZE                = {200,200};
+const sf::Vector2f BASE_SHOP_PADDING                        = {20,20};
 const sf::Vector2f BASE_SHOP_ITEM_SCALEUP_MULTIPLY_ON_HOVER = {1.2,1.2};
 
-const sf::Keyboard::Scancode SHOP_KEY_TO_MOVE_RIGHT = sf::Keyboard::Scancode::Right;
-const sf::Keyboard::Scancode SHOP_KEY_TO_MOVE_LEFT = sf::Keyboard::Scancode::Left;
+const sf::Keyboard::Scancode SHOP_KEY_TO_MOVE_RIGHT         = sf::Keyboard::Scancode::Right;
+const sf::Keyboard::Scancode SHOP_KEY_TO_MOVE_LEFT          = sf::Keyboard::Scancode::Left;
+const sf::Keyboard::Scancode SHOP_KEY_TO_OPEN_ITEM_WIDGET   = sf::Keyboard::Scancode::Z;
+const sf::Keyboard::Scancode SHOP_KEY_TO_CLOSE_ITEM_WIDGET   = sf::Keyboard::Scancode::X;
 
 class Shop : InteractiveObject
 {
+private:
+    class ItemWidget
+    {
+    private:
+        sf::Text displayNameText;
+        sf::Text statsText;
+        sf::RectangleShape rect;
+        Item::Stats stat;
+
+        bool isOpened;
+
+    public:
+        ItemWidget(sf::Vector2f widgetSize, sf::Vector2f widgetPos, sf::Vector2f displayNamePosLocal, sf::Vector2f statsTextPosLocal, sf::Font& font, uint8_t displayNameSize, uint8_t statsTextSize);
+        ~ItemWidget() = default;
+
+        void draw(sf::RenderWindow& window);
+        void update();
+        void handleEvent(const sf::Event& event);
+
+        void open();
+        void close();
+
+        void buy();
+        void cancel();
+
+        // Getters
+        bool getIsOpened();
+
+        // Setters
+        void attachItemStats(Item& item);
+        void setWidgetSize(sf::Vector2f size);
+        void setDisplayNameText(std::string str);
+        void setStatsText(std::string str);
+            // Перемещается ТОЛЬКО внутри области виджета. Позиционирование от левого-верхнего угла
+        void setDisplayNamePosition(sf::Vector2f position);
+        void setStatsTextPosition(sf::Vector2f position);
+            // Перемещает весь виджет, включая текст. Позиционирование по центру.
+        void setWidgetCenterPosition(sf::Vector2f position);
+    };
 private:
     Player* player;
     GameData* data;
 
     sf::RectangleShape shopBackground;
+    ItemWidget widget;
 
     //Grid settings
     unsigned int columns;
@@ -30,13 +72,14 @@ private:
     sf::Vector2f cellSize;
 
     // Items storage
-    std::map<std::unique_ptr<sf::Text> ,std::unique_ptr<Item>> items;
+    std::vector<std::unique_ptr<Item>> items;
 
     // Items selection iterator
-    std::map<std::unique_ptr<sf::Text> ,std::unique_ptr<Item>>::iterator itemsIt;
+    std::vector<std::unique_ptr<Item>>::iterator itemsIt;
 
     // Shop bools
     bool isOpened;
+    bool isItemWidgetOpened;
     
     // Items init
     void initializeItems();
@@ -56,6 +99,11 @@ private:
     // Заблокировать/разблокировать управление игроком
     void blockPlayerControl();
     void unblockPlayerControl();
+
+    // Открыть/закрыть вкладку с информацией покупки
+    void openSelectedItemWidget();
+    void closeSelectedItemWidget();
+
 public:
     Shop(GameData& d, Player& p, sf::Vector2f pos);
     ~Shop() = default;
@@ -70,3 +118,4 @@ public:
 
     bool getIsOpened();
 };
+
