fbc8097ebb42ba55f847e40bb151d499a765a5b7
nerealnaya-zomba <spinel1@mail.ru>
2026-01-22 18:34:18 +0700

    Player ui addCooldownRect implemention

--- src/ui/PlayerUI.cpp
+++ src/ui/PlayerUI.cpp
@@ -1,1 +1,38 @@
 #include<PlayerUI.h>
+
+void PlayerUI::updateCooldownRects()
+{
+}
+
+void PlayerUI::addCooldownRect(int& cd, sf::Texture& iconTexture)
+{
+    CooldownRect cr;
+    cr.cooldown = &cd;
+
+    cr.back.setSize(BASE_UI_COOLDOWN_RECTS_SIZE);
+    cr.back.setFillColor(BASE_UI_COOLDOWN_RECT_BACK_COLOR_ACTIVE);
+
+    cr.front.setSize(BASE_UI_COOLDOWN_RECTS_SIZE);
+    cr.front.setFillColor(BASE_UI_COOLDOWN_RECT_FRONT_COLOR);
+
+    cr.icon = std::make_unique<sf::Sprite>(iconTexture);
+
+    sf::Vector2f textureSize = static_cast<sf::Vector2f>(iconTexture.getSize());
+    sf::Vector2f calculatedScale = {
+        BASE_UI_COOLDOWN_RECTS_SIZE.x/textureSize.x,
+        BASE_UI_COOLDOWN_RECTS_SIZE.y/textureSize.y
+    };
+    cr.icon->setScale(calculatedScale);
+    setSpriteOriginToMiddle(*cr.icon);
+
+    cooldownRects.push_back(cr);
+}
+
+PlayerUI::PlayerUI(Player &p, GameCamera &c, const sf::Vector2f mnS, const sf::Vector2f mxS)
+    : minSize(mnS), maxSize(mxS)
+{
+}
+
+void PlayerUI::draw(sf::RenderWindow &window)
+{
+}
--- src/ui/PlayerUI.h
+++ src/ui/PlayerUI.h
@@ -2,10 +2,51 @@
 #include<SFML/Graphics.hpp>
 #include<Player.h>
 
+const sf::Vector2f BASE_UI_COOLDOWN_RECTS_SIZE              = {50.f,50.f};
+const sf::Color BASE_UI_COOLDOWN_RECT_BACK_COLOR_ACTIVE     = sf::Color::Green;
+const sf::Color BASE_UI_COOLDOWN_RECT_BACK_COLOR_INACTIVE   = sf::Color::Red;
+const sf::Color BASE_UI_COOLDOWN_RECT_FRONT_COLOR           = sf::Color::White;
+
+///////////////////////////////////
+// Реализует интерфейс игрока:
+// *  Здоровье
+// *  Энергия
+// 
+// *  Кулдаун телепорта
+// *  Кулдаун выстрела
+// *  Кулдаун деша
+// *  
+// 
+///////////////////////////////////
 class PlayerUI
 {
 private:
+    // Внешние объекты
+    GameCamera* camera;
+    Player* player;
+
+    
+    // Переменные для линейной интерполяции перезарядок
+    const sf::Vector2f minSize;
+    const sf::Vector2f maxSize;
+
+    // Представляет собой иконку отображающую перезарядку
+    struct CooldownRect{
+        int* cooldown;
+        sf::RectangleShape back;
+        sf::RectangleShape front;
+        std::unique_ptr<sf::Sprite> icon;
+    };
+
+    std::vector<CooldownRect> cooldownRects;
+    
+    // Обновляет все cooldownRects: текущий cooldown, back(красный если не готов, зеленый если готов), front(линейная интерполяция размера по Y в зависимости от кулдауна)
+    void updateCooldownRects();
+    void addCooldownRect(int& cd, sf::Texture& iconTexture);
 
 public:
+    PlayerUI(Player& p, GameCamera& c, const sf::Vector2f mnS, const sf::Vector2f mxS);
+    ~PlayerUI() = default;
 
+    void draw(sf::RenderWindow& window);
 };
