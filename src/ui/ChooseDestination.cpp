3311438c86bb2c7cd1b7de3faaa0c8eb4f26cfe2
nerealnaya-zomba <spinel1@mail.ru>
2026-02-11 12:51:54 +0700

    Implemented ChooseDestination

--- src/ui/ChooseDestination.cpp
+++ src/ui/ChooseDestination.cpp
@@ -1,1 +1,44 @@
+#include "GameCamera.h"
+#include "GameData.h"
+#include "GameLevel.h"
 #include<ChooseDestination.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+ChooseDestination::ChooseDestination(GameData& d, GameCamera& c, GameLevelManager& lm)
+	: data(&d), camera(&c), background(d.guiTextures.at("GUI_10.png")), manager(&lm)
+{
+	
+}
+
+void ChooseDestination::addLevelInMap(GameLevel& level,LevelDestinationRect l){
+	LevelDestinationRect lvldstrectLoc;
+
+	lvldstrectLoc.leveldestination.level =     &level;
+	lvldstrectLoc.leveldestination.isOpened =  false;
+	lvldstrectLoc.leveldestination.isVisible = true;
+	
+
+	this->levels.push_back(lvdstLoc);
+}
+
+void ChooseDestination::drawLevelDestinations(sf::RenderWindow& window){
+	for (auto& level : levels) {
+		level.						
+	}
+}
+
+void ChooseDestination::update(){
+	this->updateControls();
+}
+
+void ChooseDestination::updateControls(){
+	
+}
+
+void ChooseDestination::draw(){
+		
+}
+
+void ChooseDestination::LevelDestinationRect::draw(sf::RenderWindow& w){
+
+}
--- src/ui/ChooseDestination.h
+++ src/ui/ChooseDestination.h
@@ -1,6 +1,12 @@
 #pragma once
+#include "GameCamera.h"
+#include "GameData.h"
+#include "GameLevel.h"
 #include<SFML/Graphics.hpp>
-#include <map>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <vector>
 
 ////////////////////////
 /// Представляет собой меню для выбора места назначения телепорта
@@ -9,19 +15,58 @@
 class ChooseDestination
 {
 private:
+	// External objects pointers
+	GameData*   data;
+	GameCamera* camera;
+	GameLevelManager* manager;
+	////////////////////////////
+	
+	struct LevelDestination{
+		bool isOpened = true;
+		bool isVisible = true;
+		bool isSelected = false;
+		bool isPlayerThere = false;	
+
+		GameLevel* level;
+	};
+
+	struct LevelDestinationRect{
+
+		LevelDestination leveldestination;
+
+		sf::Sprite background;
+
+		sf::RectangleShape selectionRect;
+		
+		void draw(sf::RenderWindow& w);
+	};
+
 	sf::Sprite background;
-	std::map<std::string, sf::Sprite> levels;
-	std::map<std::string, sf::Sprite>::iterator levelIt;
+	std::vector<LevelDestinationRect> levels;
+	std::vector<LevelDestinationRect>::iterator levelIt;
 
-	bool isCalled;
+	void addLevelInMap(GameLevel& level, LevelDestinationRect l);
 
-public: 
-	////////////////////////
-	// Основые методы
-	////////////////////////
-	void updateControls();
-	void update();
-	void draw();
-	////////////////////////
+
+
+	//////////////////////////
+	/// Draw methods
+	void drawLevelDestinations(sf::RenderWindow& window);
+
+public:
+  ChooseDestination(const ChooseDestination &) = default;
+  ChooseDestination(ChooseDestination &&) = delete;
+  ChooseDestination &operator=(const ChooseDestination &) = default;
+  ChooseDestination &operator=(ChooseDestination &&) = delete;
+  ChooseDestination(GameData &d, GameCamera &c, GameLevelManager &lm);
+  ~ChooseDestination();
+
+  ////////////////////////
+  // Основые методы
+  ////////////////////////
+  void updateControls();
+  void update();
+  void draw();
+  ////////////////////////
 
 };
